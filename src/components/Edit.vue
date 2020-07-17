7e5045e8a8e3671733c2f8b4304ed29664ae474b
Rejoan <rejoan.er@gmail.com>
2020-07-17 07:36:47 +0600

    Update Edit.vue
    edit page front end done

--- src/components/Edit.vue
+++ src/components/Edit.vue
@@ -1,36 +1,96 @@
 <template>
     <div class="hello">
         <div class="container text-center">
-            <h1>Edit Image</h1>
+            <router-link :to="{ name:'Home'}"><a class="btn btn-dark" href="/Home">Home</a></router-link>
+            <h1 class="header">Edit Image</h1>
         </div>
 
         <div class="album py-5 bg-light">
             <div class="container">
+                <div class="row mb-3">
+                    <div class="col-md-6 mx-auto">
+                        <div class="text-center">
+                            <img :style="filters" class="img img-thumbnail rounded" id="main-img" :src="baseUrl+'/Gallery/'+image">
+                        </div>
+                        
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-3">
+                        <img class="img img-thumbnail rounded" @click="original()" :src="baseUrl+'/Gallery/'+image">
+                    </div>
+                    <div class="col-md-3">
+                        <img class="img img-thumbnail rounded grayscale" @click="grayscale()" :src="baseUrl+'/Gallery/'+image">
+                    </div>
+                    <div class="col-md-3">
+                        <img class="img img-thumbnail rounded sepia" @click="sepia()" :src="baseUrl+'/Gallery/'+image">
+                    </div>
+                    <div class="col-md-3">
+                        <img class="img img-thumbnail rounded invert" @click="invert()" :src="baseUrl+'/Gallery/'+image">
+                    </div>
+                </div>
                 <div class="row">
                     <div class="col-md-4">
-                        <img class="img img-thumbnail rounded" :src="baseUrl+'/Gallery/'+image">
+                       
+                    </div>
+                    <div class="col-md-8 bg-white p-2">
+                        <router-link :to="{ name:'Home'}"><a class="btn btn-outline-secondary" href="/Home">Back</a></router-link>
+                        <a class="btn btn-primary" href="">Save</a>
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-4">
-
-                </div>
-            </div>
+            
         </div>
     </div>
 </template>
-
+<style>
+    .grayscale{
+        filter: gray;
+        -webkit-filter: grayscale(1);
+        filter: grayscale(1);
+    }
+    .sepia{
+        filter: sepia(100%);
+    }
+    .invert{
+        filter: invert(100%);
+    }
+</style>
 <script>
     export default {
         name: 'Edit',
         data() {
             return {
                 image : '',
-                baseUrl : 'http://localhost/joomshaper'
+                baseUrl : 'http://localhost/joomshaper',
+                filters:{"filter":""}
             }
         },
         methods: {
+            original: function () {
+               this.filters = {
+                   
+               };
+            },
+            grayscale:function(){
+                this.filters = {
+                    "filter":"gray",
+                    "-webkit-filter":"grayscale(1)",
+                    
+                }
+                return this.filters;
+            },
+            sepia:function(){
+                this.filters = {
+                   "filter": "sepia(100%)"
+                }
+                return this.filters;
+            },
+            invert:function(){
+                this.filters = {
+                   "filter": "invert(100%)"
+                }
+            }
             
         },
         mounted(){
